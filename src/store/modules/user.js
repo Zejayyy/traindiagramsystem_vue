091004d851844077a5cd5e807fd07476b6a00478
liuzijie <liuzj1124@163.com>
2023-11-15 14:39:56 +0800

    实现登录功能

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,34 @@
-import { login, logout, getInfo } from '@/api/user'
-import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
+import { getToken, setToken, removeToken } from '@/utils/auth';
+import { login } from '@/api/user'
 
-const getDefaultState = () => {
-  return {
-    token: getToken(),
-    name: '',
-    avatar: ''
-  }
-}
-
-const state = getDefaultState()
+const state = {
+  token: getToken(),
+};
 
 const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
+  SET_TOKEN(state, token) {
+    state.token = token;
+    setToken(token); // token同步到缓存
   },
-  SET_TOKEN: (state, token) => {
-    state.token = token
+
+  REMOVE_TOKEN(state) {
+    state.token = null; // 清除vuex中的token
+    removeToken(); // 清除缓存中的token
   },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
-  }
-}
+};
 
 const actions = {
-  // user login
-  login({ commit }, userInfo) {
-    const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+  async login(context, data) {
+    const { username, password } = data
+    const token = await login({ username: username.trim(), password: password });
+    context.commit("SET_TOKEN", token);
   },
-
-  // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // user logout
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
-  }
-}
+};
 
 export default {
-  namespaced: true,
+  namespaced: true, // 开启命名空间，调用时需要用模块名user加方法名
   state,
   mutations,
-  actions
-}
+  actions,
+};
 
--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,13 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    proxy: {
+      "/api": {
+        target: "http://localhost:80",
+        pathRewrite: {'^/api':''}
+      }
+    }
+    //before: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
